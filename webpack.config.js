39fdd802540a486b9f2001e3ca1cbe581b827dd3
Richer Yang <fantasyworldidvtw@gmail.com>
2026-09-21 17:08:13 +0800

    移除未用的 webpack 設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,10 @@
 const path = require('path');
-const glob = require('glob');
-const CopyWebpackPlugin = require('copy-webpack-plugin', true);
 
 const defaultConfig = require('@wordpress/scripts/config/webpack.config', true);
 const { fromProjectRoot } = require('@wordpress/scripts/utils/file', true);
 
 const srcPath = fromProjectRoot('assets-src');
 const distPath = fromProjectRoot('assets');
-
-function getCopyPatterns() {
-    let patterns = [];
-
-    glob.sync(
-        path.join(srcPath, 'icons', '*')
-    ).forEach((file) => {
-        patterns.push({
-            from: file,
-            to: path.relative(srcPath, file)
-        });
-    });
-
-    return patterns;
-}
 
 module.exports = {
     ...defaultConfig,
@@ -34,11 +17,5 @@ module.exports = {
         ...defaultConfig.output,
         path: distPath,
         filename: '[name].js'
-    },
-    plugins: [
-        ...defaultConfig.plugins,
-        //new CopyWebpackPlugin({
-        //    patterns: getCopyPatterns()
-        //})
-    ]
+    }
 };
